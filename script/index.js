e7f12020ff0fdde10aa86188de895340c84f5c0e
Prabhu Gurung <prabhu@heubert.com>
2022-01-15 22:39:14 +0545

    cover section responsive

--- script/index.js
+++ script/index.js
@@ -9,3 +9,9 @@ function toggleSideNavMenu() {
     sideNav.classList.remove(topNavHidden);
   else sideNav.classList.add(topNavHidden);
 }
+
+// window.addEventListener("scroll", () => {
+//   let header = document.getElementsByTagName("header")[0];
+//   const opacity = parseFloat(window.scrollY / 120).toFixed(2);
+//   header.style.background = `rgba(0, 0, 0, ${opacity})`;
+// });
